aa88e149df7433621bd1754244f94952d2ee90bf
clark <cladanl@dunwoody.edu>
2026-07-10 18:56:02 -0600

    moved to MonoUI folder

--- Libs/AceConsole-3.0/AceConsole-3.0.lua
+++
@@ -1,7 +0,0 @@
--- AceConsole-3.0 Library Placeholder
--- Download Ace3 from: https://www.curseforge.com/wow/addons/ace3
-
--- This file is a placeholder. Please download the actual Ace3 library
--- and place the AceConsole-3.0 folder in MonoUI\Libs\
-
-error("AceConsole-3.0 not installed. Please download Ace3 from CurseForge and install to MonoUI\\Libs\\")
